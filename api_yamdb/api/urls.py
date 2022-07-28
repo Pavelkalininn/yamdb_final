63f92b72601d9e71aacf623ccede0e85107d6e26
Pavelel Kalinin <mono-gluk@ya.ru>
2022-07-28 22:01:49 +0300

    Docker version

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from .views import (CategoryViewSet, CommentViewSet, GenreViewSet,
+from api.views import (CategoryViewSet, CommentViewSet, GenreViewSet,
                     ReviewViewSet, SignUpView, TitleViewSet, TokenView,
                     UserViewSet)
 
